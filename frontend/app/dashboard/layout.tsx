1cd0f5aec6cf67b97bf9603d55e306fffc6fcec0
Brent Edwards <bedwards@inmusicbrands.com>
2025-07-13 07:47:50 +1200

    Fix hydration error

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import AppBreadcrumb from "@/components/breadcrumb/breadcrumb";
-import "./../globals.css"; // Your global styles
 
 import HomeButton from "@/components/buttons/HomeButton";
 import {
@@ -25,31 +24,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <SidebarProvider>
-          <AppSidebar data={mainMenu} />
-          <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-              <div className="flex items-center gap-2 px-3">
-                <SidebarTrigger />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-                <AppBreadcrumb />
-              </div>
-              <div className="flex items-center gap-2 ml-auto">
-                {" "}
-                <Separator orientation="vertical" className="h-4" />{" "}
-                <HomeButton />
-              </div>
-            </header>
-            <main className="flex-1 flex flex-col">
-              {/* {children}{" "} */}
-              {children}
-              {/* This is where your page content will be rendered */}
-            </main>
-          </SidebarInset>
-        </SidebarProvider>
-      </body>
-    </html>
+    <SidebarProvider>
+      <AppSidebar data={mainMenu} />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <AppBreadcrumb />
+          </div>
+          <div className="flex items-center gap-2 ml-auto">
+            {" "}
+            <Separator orientation="vertical" className="h-4" /> <HomeButton />
+          </div>
+        </header>
+        <main className="flex-1 flex flex-col">
+          {/* {children}{" "} */}
+          {children}
+          {/* This is where your page content will be rendered */}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
